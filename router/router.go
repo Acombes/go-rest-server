80cc589326bc0fac34893d47ceb7ee9eb47d5c77
ancom <antoine.combes.ac@gmail.com>
2016-05-24 18:26:35 +0200

    Added a twig (stick) view to display the available resources + added a path to the router to serve the stylesheets

--- router/router.go
+++ router/router.go
@@ -3,17 +3,82 @@ package router
 import (
 	"../utils"
 	"fmt"
+	"github.com/tyler-sommer/stick"
 	"net/http"
+	"strings"
 )
 
-func getNewHandler(basePath string, readFromFile func() (map[string]interface{}, error)) func(http.ResponseWriter, *http.Request) {
+//*************************************************************************************************************************************
+// Exported functions
+
+func MakeRoutes(readFromFile func() (map[string]interface{}, error)) {
+	// Recover the data from the file to build the API
+	jsonData, err := readFromFile()
+	utils.CheckError(err)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var jsonData, err = readFromFile()
+		if !utils.CheckError(err) {
+			return
+		}
+
+		tmp := []string{}
+
+		for key, value := range jsonData {
+			switch value.(type) {
+			case []interface{}:
+				tmp = append(tmp, key)
+			}
+		}
+
+		stick.NewEnv(stick.NewFilesystemLoader(".")).Execute("listing.twig", w, map[string]stick.Value{"roots": tmp})
+	})
+
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		replacer := strings.NewReplacer("/static", "static")
+
+		w.Header().Set("Content-type", "text/css")
+
+		str := replacer.Replace(r.URL.Path)
+		fmt.Println(str)
+		http.ServeFile(w, r, str)
+	})
+
+	http.HandleFunc("/db/", func(w http.ResponseWriter, r *http.Request) {
+		var jsonData, err = readFromFile()
+		if !utils.CheckError(err) {
+			return
+		}
+		prepareJSONResponse(&w)
+		fmt.Fprint(w, utils.EncodeJSON(jsonData))
+	})
+
+	// For each root key, expose a URL path
+	for key, value := range jsonData {
+		switch value.(type) {
+		case []interface{}:
+			http.HandleFunc("/"+key+"/", getNewPathHandler(key, readFromFile))
+		}
+	}
+}
+
+func StartServer(p string) {
+	// Set up the server
+	utils.LogMessage("Server startup")
+	http.ListenAndServe(p, nil)
+}
+
+//*************************************************************************************************************************************
+// Package functions
+
+func getNewPathHandler(basePath string, readFromFile func() (map[string]interface{}, error)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		jsonData, err := readFromFile()
 
 		if utils.CheckError(err) {
 			dataBit := jsonData[basePath].([]interface{})
-			utils.LogMessage(fmt.Sprint("Call to /%s/ - %d results\n", basePath, len(dataBit)))
-			w.Header().Set("Content-type", "application/json")
+			utils.LogMessage(fmt.Sprintf("Call to /%s/ - %d results\n", basePath, len(dataBit)))
+			prepareJSONResponse(&w)
 			fmt.Fprint(w, utils.EncodeJSON(dataBit))
 		} else {
 			http.Error(w, "Data source file could not be found", http.StatusInternalServerError)
@@ -21,44 +86,6 @@ func getNewHandler(basePath string, readFromFile func() (map[string]interface{},
 	}
 }
 
-func MakeRoutes(readFromFile func() (map[string]interface{}, error)) {
-	// Recover the data from the file to build the API
-	jsonData, err := readFromFile()
-	utils.CheckError(err)
-
-	// For each root key, expose a URL path
-	for key, value := range jsonData {
-		switch value.(type) {
-		case []interface{}:
-			http.HandleFunc("/"+key+"/", getNewHandler(key, readFromFile))
-		}
-	}
-
-	// Test URL path
-	http.HandleFunc("/test/", func(w http.ResponseWriter, r *http.Request) {
-		var jsonData, err = readFromFile()
-		utils.CheckError(err)
-
-		for k, v := range jsonData {
-			switch vv := v.(type) {
-			case string:
-				fmt.Fprintln(w, k, "is string", vv)
-			case float64:
-				fmt.Fprintln(w, k, "is float64", vv)
-			case []interface{}:
-				fmt.Fprintln(w, k, "is an array:")
-				for i, u := range vv {
-					fmt.Fprintln(w, i, u)
-				}
-			default:
-				fmt.Fprintln(w, k, "is of a type I don't know how to handle: ", vv)
-			}
-		}
-	})
-}
-
-func StartServer(p string) {
-	// Set up the server
-	utils.LogMessage("Server startup")
-	http.ListenAndServe(p, nil)
+func prepareJSONResponse(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-type", "application/json")
 }
